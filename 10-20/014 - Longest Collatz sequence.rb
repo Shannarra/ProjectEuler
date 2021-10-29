73dacf11cd542f23af2f5499d9cb40a8e7c55b0a
Shannarra <petarangelov15@gmail.com>
2021-10-29 12:40:35 +0200

    Solved #14

--- 10-20/014 - Longest Collatz sequence.rb
+++ 10-20/014 - Longest Collatz sequence.rb
@@ -1,3 +1,16 @@
 require_relative '../util'
 
 # https://projecteuler.net/problem=14
+
+longest_chain = [1,0] # [start, len]
+
+(1..999_999).each do |x|
+  start = x; len = 1
+  until x == 1
+    x = (x % 2 == 0) ? x/2 : 3*x + 1
+    len += 1
+  end
+  longest_chain = [start, len] if longest_chain[1] < len
+end
+
+assert_and_print longest_chain, [837799, 525]
