9dc09da3fa0b0d2f58578832d8b90ce3ad281f0f
shannarra <shannarra@shannarra-devpc.lan>
2021-11-11 15:02:41 +0200

    A fix for 022

--- 20-30/022 - Name scores.rb
+++ 20-30/022 - Name scores.rb
@@ -7,6 +7,6 @@ end
 # https://projecteuler.net/problem=22
 indexes = ('a'..'z').each_with_index.map{|l,i| [l, i+1]}.to_h
 sum = 0; names = read_asset!('20-30', "022-names")[0].split(',').sort!
-names.each_with_index {|x, ind| sum += ind * (x.chars.map{|y| (position y).abs}).reduce(:+)}
+names.each_with_index {|x, ind| sum += ind * (x.downcase.chars.map{|y| (position y).abs}).reduce(:+)}
 
 assert_and_print sum, 3099206674
